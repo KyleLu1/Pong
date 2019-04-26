d361a67280d008f8f0016cad60dfab9a3db16acc
KyleLu1 <48026700+KyleLu1@users.noreply.github.com>
2019-04-25 21:16:28 -0700

    Update Block.java

--- src/PongFiles/Block.java
+++ src/PongFiles/Block.java
@@ -3,6 +3,7 @@ package PongFiles;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
+
 import java.awt.Color;
 import java.awt.Graphics;
 
@@ -16,143 +17,80 @@ public class Block implements Locatable {
     private Color color;
 
     public Block() {
+        xPos = 0; yPos = 0; width = 0; height = 0; color = Color.BLACK;
+    }
 
-    }
- public Block(int x, int y)
-    {
-        xPos = x; yPos = y;
-    }
- 
- public Block(int x, int y, int w){
-     xPos = x; yPos = y; width = w;
- }
- 
     //add other Block constructors - x , y , width, height, color
-    public Block(int x, int y, int w, int h)
-    {
-        xPos = x; yPos = y; width = w; height = h;
+    public Block(int x, int y, int w, int h){
+        xPos = x; yPos = y; width = w; height = h; color = Color.BLACK;
     }
-    
-  
-    
-    public Block(int x, int y, int w, int h, Color c)
-    {
+    public Block(int x, int y, int w, int h, Color c){
         xPos = x; yPos = y; width = w; height = h; color = c;
     }
-    //add the other set methods
-     /**
-     * @return the xPos
-     */
-    public int getxPos() {
-        return xPos;
+    
+    public Block(int x, int y){
+        xPos = x; yPos = y; width = 10; height = 10; color = Color.BLACK;
     }
-
-    /**
-     * @param xPos the xPos to set
-     */
-    public void setxPos(int xPos) {
+    public void setPos(int x, int y){
+        xPos = x; yPos = y;
+    }
+    public void setX(int xPos) {
         this.xPos = xPos;
     }
 
-    /**
-     * @return the yPos
-     */
-    public int getyPos() {
-        return yPos;
-    }
-
-    /**
-     * @param yPos the yPos to set
-     */
-    public void setyPos(int yPos) {
+    public void setY(int yPos) {
         this.yPos = yPos;
     }
 
-    /**
-     * @return the width
-     */
-    public int getWidth() {
-        return width;
-    }
-
-    /**
-     * @param width the width to set
-     */
     public void setWidth(int width) {
         this.width = width;
     }
 
-    /**
-     * @return the height
-     */
-    public int getHeight() {
-        return height;
-    }
-
-    /**
-     * @param height the height to set
-     */
     public void setHeight(int height) {
         this.height = height;
     }
-
-    /**
-     * @return the color
-     */
-    public Color getColor() {
-        return color;
+    public void setColor(Color c){
+        color = c;
     }
-
-
-    public void setColor(Color col) {
-        this.color = col;
-    }
-
-    public void setPos(int x, int y){
-        setxPos(x);
-        setyPos(y);
-    }
-    
-    
-    
     public void draw(Graphics window) {
-        //uncomment after you write the set and get methods
         window.setColor(color);
-        window.fillRect(getxPos(), getyPos(), getWidth(), getHeight());
+        window.fillRect(getX(), getY(), getWidth(), getHeight());
     }
 
     public void draw(Graphics window, Color col) {
         window.setColor(col);
-        window.fillRect(getxPos(), getyPos(), getWidth(), getHeight());
+        window.fillRect(getX(), getY(), getWidth(), getHeight());
     }
 
     public boolean equals(Object obj) {
-        
-        Block test = (Block)obj;
-        
-        if(this.getColor()!=null && test.getColor()!=null)
-        {
-                if(this.getxPos() == test.getxPos() && this.getyPos() == test.getyPos() && this.getHeight() == test.getHeight() && this.getWidth() == test.getWidth() && this.getColor().equals(test.getColor()))
-        {
-            return true;
-        }
-        }
-        else{
-            if(this.getxPos() == test.getxPos() && this.getyPos() == test.getyPos() && this.getHeight() == test.getHeight() && this.getWidth() == test.getWidth())
-        {
-            return true;
-        }
-            
-            
-        }
-        return false;
+        Block test = (Block) obj;
+        return this.getX() == test.getX() && this.getY() == test.getY() && this.getWidth() == test.getWidth() && this.getHeight() == test.getHeight() && this.getColor().equals(test.getColor());
     }
 
-   //add the other get methods
-    //add a toString() method  - x , y , width, height, color
-    
+    public int getX() {
+        return xPos;
+    }
+
+    public int getY() {
+        return yPos;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+   //add a toString() method  - x , y , width, height, color
     public String toString(){
-        return getxPos() + " " + getyPos() + " " + getWidth() + " " + getHeight() + " " + getColor();
+        return "" + xPos + ", " + yPos + ", " + width + ", " + height + ", " + color;
     }
-
+    
+    
 }
