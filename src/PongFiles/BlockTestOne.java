8986ffc3b383627dde4c0e7f50bbc0e1a4374cf6
KyleLu1 <48026700+KyleLu1@users.noreply.github.com>
2019-04-25 21:17:06 -0700

    Update BlockTestOne.java

--- src/PongFiles/BlockTestOne.java
+++ src/PongFiles/BlockTestOne.java
@@ -7,23 +7,22 @@ package PongFiles;
 import static java.lang.System.*;
 import java.awt.Color;
 
-class BlockTestOne
-{
-	public static void main( String args[] )
-	{
-		Block one = new Block();
-		out.println(one);
+class BlockTestOne {
 
-		Block two = new Block(50,50,30,30);
-		out.println(two);
+    public static void main(String args[]) {
+        Block one = new Block();
+        out.println(one);
 
-		Block three = new Block(350,350,15,15,Color.RED);
-		out.println(three);
+        Block two = new Block(50, 50, 30, 30);
+        out.println(two);
 
-		Block four = new Block(450,50,20,60, Color.GREEN);
-		out.println(four);
-		
-		out.println(one.equals(two));
-		out.println(one.equals(one));		
-	}
+        Block three = new Block(350, 350, 15, 15, Color.RED);
+        out.println(three);
+
+        Block four = new Block(450, 50, 20, 60, Color.GREEN);
+        out.println(four);
+
+        out.println(one.equals(two));
+        out.println(one.equals(one));
+    }
 }
